716be597ec650026482adba73704db20fbf39396
xnacly <matteogropp@protonmail.com>
2022-02-11 16:34:23 +0100

    started working on loadImage

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include "libs/pgm/_pgm.h"
+#include "libs/util/_util.h"
 
 int main(void) {
-  Image *img = createImage(1920, 1080, 255);
+  //Image *img = createImage(1920, 1080, 255);
 
   //Image *img2 = copyImage(img);
   //print_image(img2, 0);
 
-  saveImage("test.pgm", img);
+  //saveImage("test.pgm", img);
+  Image *img1 = loadImage("test.pgm");
+  print_image(img1, 0);
 
-  freeImage(img);
+
+  //freeImage(img);
   return 0;
 }
