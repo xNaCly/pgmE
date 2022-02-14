632f96b8c7f891e44af28ac60c8ccede329c519d
voilacf <vio.lafe.vf@gmail.com>
2022-02-14 16:25:49 +0100

    test threshold

--- src/libs/image/_image.c
+++ src/libs/image/_image.c
@@ -9,17 +9,20 @@ void gauss(Image *img);
     //bi,j = (bi−1,j−1 + 2bi,j−1 + bi+1,j−1 + 2bi−1,j + 4bi,j + 2bi+1,j + bi−1,j+1 + 2bi,j+1 + bi+1,j+1)/16
 void laplace(Image *img);
    /*Extrahieren von Kanten
+   for (int i = 0; i < img->width ;i++){
+   for (int j = 0; j <= img->heigth ; j++){
    bi,j = bi−1,j−1 + bi,j−1 + bi+1,j−1 + bi−1,j − 8bi,j + bi+1,j + bi−1,j+1 + bi,j+1 + bi+1,j+1*/
-void threshold(Image *img, int threshold);
-    /*for (int i = 0; i <= feldgroesse ;i++){
-    for (int j = 0; j <= feldgroesse ; j++){
-    *img < threshold ? 0 : 255;}}*/
+void threshold(Image *img, int threshold){
+    for (int i = 0; i < img->width ;i++){
+    for (int j = 0; j <= img->height ; j++){
+    &img[i,j]< threshold ? 0 : 255;}}}
 void scale(Image *img, int width, int height);
-    /*createImage
+    /*createImage(width, heigth, brigthness);
+    for (int i = 0; i < img->width ;i++){
+    for (int j = 0; j <= img->height ; j++){
      ̃bi,j =(1−x)·(1−y)·bk,l +x·(1−y)·bk,l+1 +(1−x)·y·bk+1,l +x·y·bk+1,l+1*/
 void rotate(Image *img, double angle, int brigthness);
-    /*Addressen vertauschen??
-    Matrizendrehung -> cos/sin
-    rechts-/links-Drehung
+    /*
+    createImage(width, height, brigthness);
     x ̃ = cos(α) · (x − cx) − sin(α) · (y − cy) + cx
     y ̃ = sin(α) · (x − cx) + cos(α) · (y − cy) + cy*/
