05dfd406e5ddb0b4922e41b1ecc981fa1022e12f
xnacly <matteogropp@protonmail.com>
2022-03-03 14:37:05 +0100

    fix(image): fixed 2 seg faults

--- src/libs/image/_image.c
+++ src/libs/image/_image.c
@@ -1,39 +1,46 @@
 #include "_image.h"
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// TODO: Randpixel probleme beheben
+int compare(const void *a, const void *b) {
+  int int_a = *((int *)a);
+  int int_b = *((int *)b);
 
-Image *median(Image *img){
+  if (int_a == int_b) return 0;
+  else if (int_a < int_b) return -1;
+  else return 1;
+}
+
+Image *median(Image *img) {
   Image *imgCpy = copyImage(img);
-  for (int i = 0; i < imgCpy->width; i++) {
-    for (int j = 0; j < imgCpy->height; j++) {
-      int surNums[9] = {
-        img->data[i-1][j+1],
-        img->data[i][j+1],
-        img->data[i+1][j+1],
-        img->data[i-1][j],
-        img->data[i][j], // mittelpunkt der 3x3 matrix
-        img->data[i+1][j],
-        img->data[i-1][j-1],
-        img->data[i][j-1],
-        img->data[i+1][j-1]
-      }
-      // sort
-      int cmpfunc (const void * a, const void * b) {
-        return ( *(int*)a - *(int*)b );
-      };
-      int lenght = sizeof(surNums) / sizeof(int);
-      qsort(surNums, lenght, sizeof(int), compare);
-      // access median:
-      int m_pixel = surNums[5];
+  for (int i = 0; i < imgCpy->height; i++) {
+	for (int j = 0; j < imgCpy->width; j++) {
+	  if (i < 1 || j < 1 || i >= imgCpy->width - 2 || j >= imgCpy->height - 2)
+		continue;
+	  int surNums[9] = {
+		  imgCpy->data[i - 1][j + 1],
+		  imgCpy->data[i][j + 1],
+		  // TODO: seg fault here:
+		  imgCpy->data[i + 1][j + 1],
+		  imgCpy->data[i - 1][j],
+		  imgCpy->data[i][j], // mittelpunkt der 3x3 matrix
+		  imgCpy->data[i + 1][j],
+		  imgCpy->data[i - 1][j - 1],
+		  imgCpy->data[i][j - 1],
+		  imgCpy->data[i + 1][j - 1]
+	  };
+	  // sort
+	  int lenght = sizeof(surNums) / sizeof(int);
+	  qsort(surNums, lenght, sizeof(int), compare);
+	  // access median:
+	  int m_pixel = surNums[4];
 
-      // set pixel to median:
-      imgCpy->data[i][j] = m_pixel;
-    }
+	  // set pixel to median:
+	  imgCpy->data[i][j] = m_pixel;
+	}
   }
   return imgCpy;
-
 }
 
 // TODO: 
@@ -41,11 +48,11 @@ Image *gauss(Image *img) {
   Image *imgCpy = copyImage(img);
 
   for (int i = 0; i < imgCpy->width; i++) {
-    for (int j = 0; j < imgCpy->height; j++) {
-      imgCpy->data[i][j] = (imgCpy->data[i - 1][j - 1] + 2 * imgCpy->data[i][j - 1] + imgCpy->data[i + 1][j - 1] +
-                            2 * imgCpy->data[i - 1][j] + 4 * imgCpy->data[i][j] + 2 * imgCpy->data[i + 1][j] +
-                            imgCpy->data[i - 1][j + 1] + 2 * imgCpy->data[i][j + 1] + imgCpy->data[i + 1][j + 1]) / 16;
-    }
+	for (int j = 0; j < imgCpy->height; j++) {
+	  imgCpy->data[i][j] = (imgCpy->data[i - 1][j - 1] + 2 * imgCpy->data[i][j - 1] + imgCpy->data[i + 1][j - 1] +
+		  2 * imgCpy->data[i - 1][j] + 4 * imgCpy->data[i][j] + 2 * imgCpy->data[i + 1][j] +
+		  imgCpy->data[i - 1][j + 1] + 2 * imgCpy->data[i][j + 1] + imgCpy->data[i + 1][j + 1]) / 16;
+	}
   }
 
   return imgCpy;
@@ -55,53 +62,57 @@ Image *gauss(Image *img) {
 Image *laplace(Image *img) {
   Image *imgCpy = copyImage(img);
 
-  for (int i = 0; i < imgCpy->width; i++) {
-    for (int j = 0; j < imgCpy->height; j++) {
-      imgCpy->data[i][j] =
-        imgCpy->data[i - 1][j - 1] + imgCpy->data[i][j - 1] + imgCpy->data[i + 1][j - 1] + imgCpy->data[i - 1][j] -
-        8 * imgCpy->data[i][j] + imgCpy->data[i + 1][j] + imgCpy->data[i - 1][j + 1] + imgCpy->data[i][j + 1] +
-        imgCpy->data[i + 1][j + 1];
-    }
+  for (int i = 0; i < imgCpy->height; i++) {
+	for (int j = 0; j < imgCpy->width; j++) {
+	  if (i < 0 || j < 0 || i >= img->width - 2 || j >= img->height - 2)
+		continue;
+	  imgCpy->data[i][j] =
+		  imgCpy->data[i - 1][j - 1] + imgCpy->data[i][j - 1] + imgCpy->data[i + 1][j - 1] + imgCpy->data[i - 1][j] -
+			  8 * imgCpy->data[i][j] + imgCpy->data[i + 1][j] + imgCpy->data[i - 1][j + 1] + imgCpy->data[i][j + 1] +
+			  imgCpy->data[i + 1][j + 1];
+	}
   }
 
   return imgCpy;
 }
 
-
 Image *threshold(Image *img, int threshold) {
   if (threshold < 1) {
-    printf("threshold was too small, was set to: 1");
-    threshold = 1;
+	printf("threshold was too small, was set to: 1");
+	threshold = 1;
   } else if (threshold > MAX_BRIGHT) {
-    printf("threshold was too big, was set to: 255");
-    threshold = 255;
+	printf("threshold was too big, was set to: 255");
+	threshold = 255;
   }
 
   Image *imgCpy = copyImage(img);
 
   for (int i = 0; i < imgCpy->height; i++) {
-    for (int j = 0; j < imgCpy->width; j++) {
-      imgCpy->data[i][j] = (imgCpy->data[i][j] < threshold) ? 0 : 255;
-    }
+	for (int j = 0; j < imgCpy->width; j++) {
+	  imgCpy->data[i][j] = (imgCpy->data[i][j] < threshold) ? 0 : 255;
+	}
   }
 
   return imgCpy;
 }
 
+// TODO: BIG WIP
 Image *scale(Image *img, int width, int height) {
-  Image *newImg = createImage(width,height, 0);
+  Image *newImg = createImage(width, height, 0);
 
   for (int i = 0; i < newImg->width; i++) {
-    for (int j = 0; j < newImg->height; j++) {
-      for (int k = 0; k < img->width; k++){
-        for (int l = 0; l < img->width; l++){
-        newImg->data[i][j] = (1-img->width) * (1-(img->height)) * (img->data[k][l]) + (img->width) * (1-(img->height)) * (img->data[k][l+1]) 
-        + (1-(img->width)) * (img->height) * (img->data[k+1][l]) + (img->width) * (img->height) * (img->data[k+1][l+1]); 
-        }
-      }
-    }
+	for (int j = 0; j < newImg->height; j++) {
+	  for (int k = 0; k < img->width; k++) {
+		for (int l = 0; l < img->width; l++) {
+		  newImg->data[i][j] = (1 - img->width) * (1 - (img->height)) * (img->data[k][l])
+			  + (img->width) * (1 - (img->height)) * (img->data[k][l + 1])
+			  + (1 - (img->width)) * (img->height) * (img->data[k + 1][l])
+			  + (img->width) * (img->height) * (img->data[k + 1][l + 1]);
+		}
+	  }
+	}
   }
- 
+
   return newImg;
 }
 
@@ -113,7 +124,7 @@ Image *rotate(Image *img, double angle, int brigthness) {
   /*int new_width = (int) (img->width * cos(angle) + img->height * cos(90 - angle));
   int new_height = (int) (img->width * sin(angle) + img->height * sin(90 - angle));*/
 
- /* Image *imgCpy = createImage(new_width, new_height, brigthness);*/
+  /* Image *imgCpy = createImage(new_width, new_height, brigthness);*/
   Image *imgCpy = createImage(img->width, img->height, brigthness);
 
   // calculate center
@@ -121,16 +132,16 @@ Image *rotate(Image *img, double angle, int brigthness) {
   int y_mid = img->height / 2;
 
   for (int y = 0; y < img->height; y++) {
-    for (int x = 0; x < img->width; x++) {
-      // calculate new positions
-      int x_new = (int) round(cos(angle) * (x - x_mid) - sin(angle) * (y - y_mid) + x_mid);
-      int y_new = (int) round(sin(angle) * (x - x_mid) + cos(angle) * (y - y_mid) + y_mid);
-      if (x_new == img->width) x_new = 0;
-      if (y_new == img->height) y_new = 0;
-      if (x_new < 0 || y_new < 0 || x_new >= img->width || y_new >= img->height)
-        continue;
-      imgCpy->data[y_new][x_new] = img->data[y][x]; //TODO: doesnt work, seg fault
-    }
+	for (int x = 0; x < img->width; x++) {
+	  // calculate new positions
+	  int x_new = (int)round(cos(angle) * (x - x_mid) - sin(angle) * (y - y_mid) + x_mid);
+	  int y_new = (int)round(sin(angle) * (x - x_mid) + cos(angle) * (y - y_mid) + y_mid);
+	  if (x_new == img->width) x_new = 0;
+	  if (y_new == img->height) y_new = 0;
+	  if (x_new < 0 || y_new < 0 || x_new >= img->width || y_new >= img->height)
+		continue;
+	  imgCpy->data[y_new][x_new] = img->data[y][x]; //TODO: doesnt work, seg fault
+	}
   }
 
   return imgCpy;
